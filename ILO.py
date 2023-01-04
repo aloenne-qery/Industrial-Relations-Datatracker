2a845d8d86297df8b988f0ad9ead474741e35a59
aloenne-qery <alf.lonne@qery.no>
2023-01-04 09:59:36 +0100

    script for ILO data

--- ILO.py
+++ ILO.py
@@ -8,16 +8,15 @@ import io
 import pandas as pd
 import pycountry
 os.makedirs('data', exist_ok=True)
-access_token = os.getenv('DW_TOKEN')
 
 #Trade Union Density Rate Worldwide
 df = pd.read_csv('https://www.ilo.org/sdmx/rest/data/ILO,DF_ILR_TUMT_NOC_RT/?format=csv&startPeriod=2010&lastNObservations=1')
 country_codes = {country.alpha_3: country.name for country in pycountry.countries}
 df['country_name'] = df['REF_AREA'].map(country_codes)
-df.to_csv('data/Trade_Union_Density_Rate_ILO.csv', index=True)
+df.to_csv('data/Trade_Union_Density_Rate_ILO_Latest.csv', index=True)
 
 #Collective Bargaining Coverage Rate Worldwide
 df = pd.read_csv('https://www.ilo.org/sdmx/rest/data/ILO,DF_ILR_CBCT_NOC_RT/?format=csv&startPeriod=2010-01-01&lastNObservations=1')
 country_codes = {country.alpha_3: country.name for country in pycountry.countries}
 df['country_name'] = df['REF_AREA'].map(country_codes)
-df.to_csv('data/Collective_Bargaining_Rate_ILO.csv', index=True)
+df.to_csv('data/Collective_Bargaining_Rate_ILO_Latest.csv', index=True)
