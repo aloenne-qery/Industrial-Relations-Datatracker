c310a9afc2034642a4a2c24de58334cc2aa1f81f
aloenne-qery <alf.lonne@qery.no>
2023-01-04 08:29:36 +0100

    Trade Union Density and Collective Bargaining Rate Share

---
+++ ilo.py
@@ -0,0 +1,23 @@
+from pyjstat import pyjstat
+import requests
+import os
+import json
+from datetime import datetime
+import locale
+import io
+import pandas as pd
+import pycountry
+os.makedirs('data', exist_ok=True)
+access_token = os.getenv('DW_TOKEN')
+
+#Trade Union Density Rate Worldwide
+df = pd.read_csv('https://www.ilo.org/sdmx/rest/data/ILO,DF_ILR_TUMT_NOC_RT/?format=csv&startPeriod=2010&lastNObservations=1')
+country_codes = {country.alpha_3: country.name for country in pycountry.countries}
+df['country_name'] = df['REF_AREA'].map(country_codes)
+df.to_csv('data/Trade_Union_Density_Rate_ILO_Latest.csv', index=True)
+
+#Collective Bargaining Coverage Rate Worldwide
+df = pd.read_csv('https://www.ilo.org/sdmx/rest/data/ILO,DF_ILR_CBCT_NOC_RT/?format=csv&startPeriod=2010-01-01&lastNObservations=1')
+country_codes = {country.alpha_3: country.name for country in pycountry.countries}
+df['country_name'] = df['REF_AREA'].map(country_codes)
+df.to_csv('data/Collective_Bargaining_Rate_ILO_Latest.csv', index=True)
