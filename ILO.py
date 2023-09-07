1c94bdfad4f8c41dba156adb8d1aafa9cddc9c8d
aloenne-qery <alf.lonne@qery.no>
2023-09-07 09:14:34 +0200

    add data for all years

--- ILO.py
+++ ILO.py
@@ -9,14 +9,28 @@ import pandas as pd
 import pycountry
 os.makedirs('data', exist_ok=True)
 
-#Trade Union Density Rate Worldwide
+#Trade Union Density Rate Worldwide Latest Year
 df = pd.read_csv('https://www.ilo.org/sdmx/rest/data/ILO,DF_ILR_TUMT_NOC_RT/?format=csv&startPeriod=2010&lastNObservations=1')
 country_codes = {country.alpha_3: country.name for country in pycountry.countries}
 df['country_name'] = df['REF_AREA'].map(country_codes)
 df.to_csv('data/Trade_Union_Density_Rate_ILO_Latest.csv', index=True)
 
-#Collective Bargaining Coverage Rate Worldwide
+#Trade Union Density Rate Worldwide All Years
+df = pd.read_csv('https://www.ilo.org/sdmx/rest/data/ILO,DF_ILR_TUMT_NOC_RT/?format=csv')
+country_codes = {country.alpha_3: country.name for country in pycountry.countries}
+df['country_name'] = df['REF_AREA'].map(country_codes)
+df_new = df.pivot(index='country_name', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.to_csv('data/Trade_Union_Density_Rate_ILO_All_Years.csv', index=True)
+
+#Collective Bargaining Coverage Rate Worldwide Latest Year
 df = pd.read_csv('https://www.ilo.org/sdmx/rest/data/ILO,DF_ILR_CBCT_NOC_RT/?format=csv&startPeriod=2010-01-01&lastNObservations=1')
 country_codes = {country.alpha_3: country.name for country in pycountry.countries}
 df['country_name'] = df['REF_AREA'].map(country_codes)
 df.to_csv('data/Collective_Bargaining_Rate_ILO_Latest.csv', index=True)
+
+#Collective Bargaining Coverage Rate Worldwide All Years
+df = pd.read_csv('https://www.ilo.org/sdmx/rest/data/ILO,DF_ILR_CBCT_NOC_RT/?format=csv')
+country_codes = {country.alpha_3: country.name for country in pycountry.countries}
+df['country_name'] = df['REF_AREA'].map(country_codes)
+df_new = df.pivot(index='country_name', columns='TIME_PERIOD', values='OBS_VALUE')
+df.to_csv('data/Collective_Bargaining_Rate_ILO_All_Years.csv', index=True)
